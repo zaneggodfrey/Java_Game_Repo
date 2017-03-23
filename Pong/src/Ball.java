7100d7f5893a6335ef6e97df03b104030b42666d
zaneggodfrey <zanegodfrey@gmail.com>
2017-03-22 19:18:22 -0700

    Update Ball.java

--- Pong/src/Ball.java
+++ Pong/src/Ball.java
@@ -1,17 +1,27 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
 
 public class Ball {
-	int ballPosition;
-	// Constructs the class
-	public Ball() {
-		
+	int size = Home.width/40;
+	double x;
+	double y;
+	double xvelocity;
+	double yvelocity;
+	public Ball(){
+		xvelocity=0;
+		yvelocity=0;
+		x=Home.width/2 - size/2;
+		y=Home.height/2 - size/2;
 	}
-	
-	public void setBallPosition(int space) {
-		ballPosition = space;
+	public void update(){
+		//called 40 times a second
 	}
-	
-	public int getBallPosition() {
-		return ballPosition;
+	public void collided(Paddle paddle){
+		//called when ball interacts with paddle
 	}
-	
+	public void paint(Graphics g){
+		g.setColor(Color.WHITE);
+		g.fillRect((int)x, (int)y, size, size);
+	}
 }
